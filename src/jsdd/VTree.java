b7593523971a9db28b6f56f25f0b3e2576486021
Ricardo Herrmann <ricardo.herrmann@br.ibm.com>
2012-11-18 19:11:10 -0200

    Helper method for right-linear vtree creation from named variables

--- src/jsdd/VTree.java
+++ src/jsdd/VTree.java
@@ -22,4 +22,20 @@ public abstract class VTree implements StringBuildable {
 		return toStringBuilder().toString();
 	}
 
+	public static VTree buildRightLinear(final VariableRegistry vars, final String... names) {
+		if (names.length == 0) {
+			throw new IllegalArgumentException("List of variable names cannot be empty");
+		}
+		return buildRightLinear(vars, 0, names);
+	}
+
+	private static VTree buildRightLinear(final VariableRegistry vars, final int current, final String... names) {
+		final LeafNode leaf = new LeafNode(vars.register(names[current]));
+		if (names.length - current == 1) {
+			return leaf;
+		} else {
+			return new InternalNode(leaf, buildRightLinear(vars, current + 1, names));
+		}
+	}
+
 }
