cd4b6461775f344b0e062f58c651622dea2ea1eb
Ricardo Herrmann <ricardo.herrmann@br.ibm.com>
2013-01-11 14:48:48 -0200

    Generic partitioning iterator

--- src/jsdd/vtree/DissectionIterator.java
+++ src/jsdd/vtree/DissectionIterator.java
@@ -1,83 +1,25 @@
 package jsdd.vtree;
 
-import java.util.Iterator;
-import java.util.NoSuchElementException;
-
-import util.SingleElementIterator;
+import util.PartitioningIterator;
 
 /**
  * Lazily generates tree dissections.
  * 
  * @author Ricardo Herrmann
  */
-public class DissectionIterator implements Iterator<Tree>, Iterable<Tree> {
-
-	private final Tree[] leaves;
-	private final int begin, end;
-
-	private int sep;
-
-	private Iterator<Tree> left, right;
-
-	private Tree leftTree;
+public class DissectionIterator extends PartitioningIterator<Tree> {
 
 	public DissectionIterator(final Tree[] leaves) {
-		this(leaves, 0, leaves.length);
-	}
-
-	private DissectionIterator(final Tree[] leaves, final int begin, final int end) {
-		this.leaves = leaves;
-		this.begin = begin;
-		this.end = end;
-		sep = begin + 1;
-		left = buildIterator(begin, sep);
-		right = buildIterator(sep, end);
-		leftTree = left.next();
-	}
-
-	private Iterator<Tree> buildIterator(final int begin, final int end) {
-		if (end - begin == 1) {
-			return new SingleElementIterator<Tree>(leaves[begin]);
-		} else {
-			return new DissectionIterator(leaves, begin, end);
-		}
-	}
-
-	@Override
-	public boolean hasNext() {
-		return right.hasNext() || left.hasNext() || sep < end - 1;
-	}
-
-	@Override
-	public Tree next() {
-		if (right.hasNext()) {
-			final Tree rightTree = right.next();
-			if (end < leaves.length) {
-				return new InternalVTree((VTree) leftTree, (VTree) rightTree);
-			} else {
-				return new InternalAVTree((VTree) leftTree, (AVTree) rightTree);
+		super(leaves, new PartitioningIterator.Combiner<Tree>() {
+			@Override
+			public Tree combine(final Tree left, final Tree right, final Tree[] leaves, final int begin, final int end) {
+				if (end < leaves.length) {
+					return new InternalVTree((VTree) left, (VTree) right);
+				} else {
+					return new InternalAVTree((VTree) left, (AVTree) right);
+				}
 			}
-		} else if (left.hasNext()) {
-			leftTree = left.next();
-			right = buildIterator(sep, end);
-			return next();
-		} else if (sep < end - 1) {
-			sep++;
-			left = buildIterator(begin, sep);
-			return next();
-		} else {
-			throw new NoSuchElementException();
-		}
-	}
-
-	@Override
-	public void remove() {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public Iterator<Tree> iterator() {
-		return this;
+		});
 	}
 
 }
---
+++ src/util/PartitioningIterator.java
@@ -0,0 +1,85 @@
+package util;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Base class for lazily generating hierarchical set partitions. Implementations
+ * must provide a combiner for joining the left and right sides of partitions.
+ * 
+ * @author Ricardo Herrmann
+ */
+public class PartitioningIterator<T> implements Iterator<T>, Iterable<T> {
+
+	private final T[] elements;
+	private final int begin, end;
+
+	private int sep;
+
+	private Iterator<T> left, right;
+
+	private T leftPartition;
+
+	public interface Combiner<T> {
+		T combine(T leftPartition, T rightPartition, final T[] elements, int begin, int end);
+	}
+
+	private Combiner<T> combiner;
+
+	public PartitioningIterator(final T[] elements, final Combiner<T> combiner) {
+		this(elements, 0, elements.length, combiner);
+	}
+
+	private PartitioningIterator(final T[] elements, final int begin, final int end, final Combiner<T> combiner) {
+		this.elements = elements;
+		this.begin = begin;
+		this.end = end;
+		this.combiner = combiner;
+		sep = begin + 1;
+		left = buildIterator(begin, sep);
+		right = buildIterator(sep, end);
+		leftPartition = left.next();
+	}
+
+	private Iterator<T> buildIterator(final int begin, final int end) {
+		if (end - begin == 1) {
+			return new SingleElementIterator<T>(elements[begin]);
+		} else {
+			return new PartitioningIterator<T>(elements, begin, end, combiner);
+		}
+	}
+
+	@Override
+	public boolean hasNext() {
+		return right.hasNext() || left.hasNext() || sep < end - 1;
+	}
+
+	@Override
+	public T next() {
+		if (right.hasNext()) {
+			final T rightPartition = right.next();
+			return combiner.combine(leftPartition, rightPartition, elements, begin, end);
+		} else if (left.hasNext()) {
+			leftPartition = left.next();
+			right = buildIterator(sep, end);
+			return next();
+		} else if (sep < end - 1) {
+			sep++;
+			left = buildIterator(begin, sep);
+			return next();
+		} else {
+			throw new NoSuchElementException();
+		}
+	}
+
+	@Override
+	public void remove() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public Iterator<T> iterator() {
+		return this;
+	}
+
+}
