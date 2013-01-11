d7726ba868d1fb91505fb8c23fbf28ae5d70111d
Ricardo Herrmann <ricardo.herrmann@br.ibm.com>
2013-01-11 16:25:30 -0200

    Javadoc fix

--- src/jasdd/util/StringBuildable.java
+++ src/jasdd/util/StringBuildable.java
@@ -2,7 +2,7 @@ package jasdd.util;
 
 /**
  * Type of classes that provide human-readable serialization of objects.
- * Implementations should also overwrite {@link Object.toString}. 
+ * Implementations should also overwrite {@link Object#toString()}. 
  * 
  * @author Ricardo Herrmann
  */
